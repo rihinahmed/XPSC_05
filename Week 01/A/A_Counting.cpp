6eb6f098f45eb78b89ba0edf6b9a893afe2fa048
Md. Mahfuz Ahmed Rihin <mdmahfuzahmedrihin@gmail.com>
2024-11-03 19:57:55 +0600

    Add files via upload

---
+++ Week 01/A/A_Counting.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define spc " "
+#define pf push_front
+#define endl "\n"
+#define YES cout << "YES" << endl
+#define NO cout << "NO" << endl
+#define rihin                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+using namespace std;
+int main()
+{
+    rihin;
+    int a,b;
+    cin>>a>>b;
+    int ans = b-a+1;
+    if(ans<=0) cout<<0<<endl;
+    else cout<<ans<<endl;   
+}
